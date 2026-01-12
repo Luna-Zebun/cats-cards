7dbc8c3e195befb87783a5b9e305454d04434db7
Your Name <you@example.com>
2026-01-13 00:29:34 +0300

    adding - cats not found- feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { useState, useEffect } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import CardList from './components/card list/card-list.component';
 import SearchBox from './components/search box/search_box.component';
 import './App.css';
 
 const App =() =>{
   const [cats, setCats] = useState([]);
-  const [filteredCats, setFilteredCats] = useState([]); /// filteredCats : []
+  const [filteredCats, setFilteredCats] = useState([]); /// filteredCats : []  
+  const [ CardState, setCardState]=useState(false)
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((Response) => Response.json())
@@ -19,14 +18,31 @@ const App =() =>{
     const OnSearch=(event) => {
       const searchString=event.target.value.toLowerCase(); 
       const newFilteredCats=cats.filter(( cat ) => cat.name.toLowerCase().includes(searchString)); ///newFilteredCats: [] 
-      setFilteredCats(newFilteredCats);
+      // console.log(newFilteredCats)
+        setFilteredCats(newFilteredCats);
+
+      if (searchString && newFilteredCats.length===0){
+        setCardState(true);
+      }
+      else{
+        setCardState(false);
+      }
     };
+  let value;
+
+  if (CardState) {
+    value = <h2 className='message'>Cats Not Found</h2>;
+  }
+  else {
+    value= <CardList cats={filteredCats} />;
+  }
+
   return (
     <div className='app-container'> 
       <h1 className='app-title'>Cats Rolodex</h1>
       <SearchBox ChangeHandler={OnSearch} />
-      <CardList cats={ filteredCats } />
-      
+      {value}
+
     </div>
 
   );
